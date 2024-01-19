e429ea6814e0a9ca7c76a41145c665d17c91970b
DaviAieta <aieta.davi@gmail.com>
2024-01-19 00:12:33 -0300

    Create index.js

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 import http from "node:http"
+
+const port = 3000
+
+const server = http.createServer((req, res) =>{
+    const { url, method } = req
+
+    if(url == '/' && method == 'GET'){
+        return res.end(JSON.stringify({
+            status: 200,
+            body: '200 OK'
+        }
+        ))
+    }
+
+    res.statusCode = 404
+    return res.end(
+        JSON.stringify({
+            status: 404,
+            body: '404 (not found)'
+        })
+    )
+})
+
+server.listen(port, () =>{
+    console.log('Server is running, port: ' + port)
+})
